f14c58e8d9a61a3c768fa1d4964ec9fbed15bf62
Ulascan Sarica <ulascan90@gmail.com>
2024-01-05 05:50:37 +0100

    inlines

--- interface/std_ivy/memory/IvyAddressof.h
+++ interface/std_ivy/memory/IvyAddressof.h
@@ -13,31 +13,24 @@ namespace std_ivy{
  || ((COMPILER == COMPILER_GCC) && (COMPILER_VERSION >= 70000)) \
  || (COMPILER == COMPILER_MSVC) \
  || (COMPILER == COMPILER_NVHPC)
-  template<typename T> inline __CUDA_HOST_DEVICE__ __CPP_CONSTEXPR__
-    T*
-    addressof(T& v) __NOEXCEPT__{
-    return __builtin_addressof(v);
-  }
+  template<typename T>
+  __INLINE_FCN_RELAXED__ __CUDA_HOST_DEVICE__ __CPP_CONSTEXPR__ T* addressof(T& v) __NOEXCEPT__{ return __builtin_addressof(v); }
 #else
   template<typename T>
-  inline __CUDA_HOST_DEVICE__ std_ttraits::enable_if_t<std_ttraits::is_object_v<std_ttraits::remove_reference_t<T>>, T*>
-    addressof(T& v) __NOEXCEPT__{
-    return __REINTERPRET_CAST__(T*, &__CONST_CAST__(char&, __REINTERPRET_CAST__(const volatile char&, v)));
-  }
+  __INLINE_FCN_RELAXED__ __CUDA_HOST_DEVICE__ std_ttraits::enable_if_t<std_ttraits::is_object_v<std_ttraits::remove_reference_t<T>>, T*>
+    addressof(T& v) __NOEXCEPT__{ return __REINTERPRET_CAST__(T*, &__CONST_CAST__(char&, __REINTERPRET_CAST__(const volatile char&, v))); }
   template<typename T>
-  inline __CUDA_HOST_DEVICE__ std_ttraits::enable_if_t<!std_ttraits::is_object_v<std_ttraits::remove_reference_t<T>>, T*>
-    addressof(T& v) __NOEXCEPT__{
-    return &v;
-  }
+  __INLINE_FCN_RELAXED__ __CUDA_HOST_DEVICE__ std_ttraits::enable_if_t<!std_ttraits::is_object_v<std_ttraits::remove_reference_t<T>>, T*>
+    addressof(T& v) __NOEXCEPT__{ return &v; }
 #endif
 
 #define __OBJC_POINTER_CMD__(__OBJ_PTR_TYPE__) \
-  template<typename T> inline __CUDA_HOST_DEVICE__ __OBJ_PTR_TYPE__ T* addressof(__OBJ_PTR_TYPE__ T& v) __NOEXCEPT__{ return &v; }
+  template<typename T> __INLINE_FCN_RELAXED__ __CUDA_HOST_DEVICE__ __OBJ_PTR_TYPE__ T* addressof(__OBJ_PTR_TYPE__ T& v) __NOEXCEPT__{ return &v; }
   __OBJC_POINTER_CMDS__;
 #undef __OBJC_POINTER_CMD__
 
-  template<typename T> inline __CUDA_HOST_DEVICE__ T* addressof(const T&&) = delete;
-  template<typename T> inline __CUDA_HOST_DEVICE__ const T* addressof(const T&&) = delete;
+  template<typename T> __INLINE_FCN_RELAXED__ __CUDA_HOST_DEVICE__ T* addressof(const T&&) = delete;
+  template<typename T> __INLINE_FCN_RELAXED__ __CUDA_HOST_DEVICE__ const T* addressof(const T&&) = delete;
 }
 
 #endif
