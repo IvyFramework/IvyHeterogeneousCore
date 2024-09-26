c3e999c30c6a92b7c893443c3741f7da14bac6e3
usarica <ulascan90@gmail.com>
2024-09-26 10:21:45 +0000

    Helper structs 2

--- interface/IvySecrets.h
+++ interface/IvySecrets.h
@@ -1,9 +1,13 @@
 #ifndef IVYSECRETS_H
 #define IVYSECRETS_H
 
+
+#include "config/IvyCompilerConfig.h"
+
+
 namespace IvySecrets{
   struct dump_helper{
-    template<typename T> static void dump(T& obj){
+    template<typename T> static __HOST_DEVICE__ void dump(T& obj){
       obj.dump();
     }
   };
